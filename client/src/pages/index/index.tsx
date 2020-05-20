df890919dbd54704f57cd9c8506e44f0a0c63e18
kisstar <dwh.chn@foxmail.com>
2020-05-20 14:34:37 +0800

    feat: add taro-ui

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
 
 import Login from '../../components/login/index'
@@ -31,6 +32,7 @@ export default class Index extends Component {
     return (
       <View className='index'>
         <Login/>
+        <AtButton type='primary'>按钮文案</AtButton>
       </View>
     )
   }
